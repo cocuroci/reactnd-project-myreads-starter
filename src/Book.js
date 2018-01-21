d485544c0f3fad0fb1594f7bf72a5fcf1527935b
André Cocuroci <andre.cocuroci@ebanx.com>
2018-01-21 17:47:12 -0200

    feat: Dados do livro
    Inserido os dados de cada livro.

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import * as Shelves from './Shelves'
 
-const Book = () => (
+const Book = ({ book }) => (
     <div className="book">
         <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
             <div className="book-shelf-changer">
                 <select>
                     <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
+                    <option value="currentlyReading" selected={book.shelf === Shelves.CURRENTLY_READING}>Currently Reading</option>
+                    <option value="wantToRead" selected={book.shelf === Shelves.WANT_TO_READ}>Want to Read</option>
+                    <option value="read" selected={book.shelf === Shelves.READ}>Read</option>
                     <option value="none">None</option>
                 </select>
             </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join('\n')}</div>
     </div>
 )
 
