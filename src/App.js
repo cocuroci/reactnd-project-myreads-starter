5fcf4f9021f29337b9614de5d0eb0956575ebcd8
André Cocuroci <andre.cocuroci@ebanx.com>
2018-01-21 14:27:06 -0200

    feat: Configuração Router
    Configuração inicial do Router na aplicação.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     showSearchPage: false
   }
 
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
+        <div className="app">
+            <Route exact path="/" />
+            <Route path="/search" />
+        </div>
+        /*{this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
@@ -28,7 +26,7 @@ class BooksApp extends React.Component {
 
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
-                */}
+                }
                 <input type="text" placeholder="Search by title or author"/>
 
               </div>
@@ -198,7 +196,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}
-      </div>
+      </div>*/
     )
   }
 }
